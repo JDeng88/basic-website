4cf1dd8e8157b1c65b31c96220bb9cdd6a921ec9
Darren Wang <rardenawng@gmail.com>
2020-07-12 13:20:52 -0400

    new

---
+++ authentication.js
@@ -0,0 +1,11 @@
+const checkAuth = function(req, res, next){
+    if (!req.session.user_id) {
+      res.redirect('/');
+    } else {
+      next();
+    }
+}
+
+module.exports = {
+    checkAuth : checkAuth
+};
