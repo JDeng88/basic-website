94ee3e9f484614879b22df5a86fba3c385a1de00
Darren Wang <rardenawng@gmail.com>
2020-07-12 16:30:38 -0400

    added mongodb test

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-const express = require('express');                 // returns a method using express
-const app = express();                              // gets an express object from the mthod
-const path = require('path');                       // provides path functions
-const expressSession = require('express-session');  // allows create sessions, cookies
-const bodyParser = require('body-parser');          // allows to retrieve information from html items
+/*
+ *  Date Created: July 12, 2020
+ *  Author: Justin Deng, Yuxuan Zou, Darren Wang
+ *  Description: Login Page powered by MongoDB and incorporates password encryption
+ */
 
-const checkAuth = require('./authentication.js').checkAuth; // custome method from authentication.js
+const express = require('express');                         // returns a method using express
+const app = express();                                      // gets an express object from the mthod
+const path = require('path');                               // provides path functions
+const expressSession = require('express-session');          // allows create sessions, cookies
+const bodyParser = require('body-parser');                  // allows to retrieve information from html items
+const assert = require('assert');                           // testing for any errors
+const MongoClient = require('mongodb').MongoClient;         // returns a MongoDB client
+const checkAuth = require('./authentication.js').checkAuth; // custom method from authentication.js for authentication
+
+// Login information for MongoDB
+const MONGOD_USER = "madproductive";
+const MONGOD_PWD = "wewillpushforward";
+const DB_NAME = "test";
+const uri = "mongodb+srv://"+MONGOD_USER+":"+MONGOD_PWD+
+            "@cluster0.jd51y.mongodb.net/"+DB_NAME+"?retryWrites=true&w=majority";
+
 
 app.use(bodyParser.urlencoded({ extended: true })); 
-app.use(expressSession({secret: '/dashboard', saveUninitialized: true, resave: false}));
-app.listen(3000); // hosted on port 3000
+// Set dashboard to only be accessed if authorized
+app.use(expressSession({secret : '/dashboard', 
+                        saveUninitialized: true, 
+                        resave : false})); 
+app.listen(3000);       // hosted on port 3000
 
 // temporary username and password for testing
-var realUser;
+var realUser; 
 var realPwd;
 
-// GET REQUESTS, Linking html to pages
+// GET REQUESTS, Linking html to pages ====================================================================
 // req => sending from client
 // res => recieving from server
 app.get('/', function(req,res){
@@ -30,7 +48,7 @@ app.get('/register', function(req,res){
     res.sendFile(path.join(__dirname+'/static/register.html'));
 });
 
-// POST REQUESTS
+// POST REQUESTS ===========================================================================================
 app.post('/login_form', function(req,res){
     var user = req.body.log_user;
     var pwd = req.body.log_pwd;
@@ -46,4 +64,37 @@ app.post('/register_form', function(req,res){
     realUser = req.body.reg_user;
     realPwd = req.body.reg_pwd;
     res.redirect('/');
+});
+
+// testing MongoDB connection
+app.post('/connect', function(req,res){
+    res.redirect('/testing_mongodb');
+});
+
+app.get('/testing_mongodb', function(req,res){
+    console.log("Welcome to testing platform for MongoDB!");
+    
+    var myobj = { name: "Company Inc", address: "Highway 37" };
+    const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
+    client.connect(err => {
+        if(err) return err;
+        console.log("Connected to MongoDB!");
+        const collection = client.db("test").collection("devices");
+        collection.insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("Inserted Company Inc");
+        });
+        console.log("Attempting to retrieve one entry...");
+        collection.findOne({}, function(err, result) {
+            if (err) throw err;
+            console.log('Entry name retrieved is: ' + result.name);
+        });
+        
+        // count the number 
+        collection.countDocuments({}, function(error, numOfDocs) {
+            console.log('I have '+numOfDocs+' documents in my collection');
+        });
+
+    });
+    client.close();
 });
